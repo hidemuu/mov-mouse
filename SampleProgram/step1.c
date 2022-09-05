5ccab299231a3982e29a75419bbd8517a8c22f83
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-09-05 23:31:34 +0900

    20220905

---
+++ SampleProgram/step1.c
@@ -0,0 +1,27 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void) {
+    int led[4];
+    int i;
+
+    led[0] = open("/dev/rtled0", O_WRONLY);
+    led[1] = open("/dev/rtled1", O_WRONLY);
+    led[2] = open("/dev/rtled2", O_WRONLY);
+    led[3] = open("/dev/rtled3", O_WRONLY);
+
+    while (1) {
+        for (i = 0; i < 4; i++) {
+            write(led[i], "1", 1);
+        }
+        usleep(500 * 1000);
+        for (i = 0; i < 4; i++) {
+            write(led[i], "0", 1);
+        }
+        usleep(500 * 1000);
+    }
+    for (i = 0; i < 4; i++) {
+        close(led[i]);
+    }
+    return 0;
+}
---
+++ src/script/leds_off.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import time
+import sys
+
+files = ["/dev/rtled0", "/dev/rtled1", "/dev/rtled2", "/dev/rtled3"]
+
+for filename in files:
+        with open(filename, 'w') as f:
+            f.write("0")
